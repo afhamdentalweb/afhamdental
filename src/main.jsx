fce6da5cc4c8eb64700dc417313c864f3e35ec92
afhamdental <afhamdental.web@gmail.com>
2023-08-03 08:16:10 +0700

    Navigate Home to /dsp-white-clinic-35-red

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import DSPWhiteClinic35Red from "./pages/DSPWhiteClinic35Red.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
@@ -14,7 +18,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Home />,
+        element: <Navigate to="/dsp-white-clinic-35-red" />,
       },
       {
         path: "/dsp-white-clinic-35-red",
