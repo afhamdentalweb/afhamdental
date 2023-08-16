c43744d20617522b2a0ccf6b289cda3e3379c563
afhamdental <afhamdental.web@gmail.com>
2023-08-16 22:41:23 +0700

    Revert "Make order button not static"
    This reverts commit 0a8dcaa62ee85b0d33d9266d6cf0adca21d70d39.

--- src/components/DSPWhiteClinic35Red/HowToUse.jsx
+++ src/components/DSPWhiteClinic35Red/HowToUse.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const HowToUse = () => {
   return (
     <div className="bg-blue-pattern bg-cover">
-      <div className="container mx-auto flex flex-col py-28 px-5 space-y-12 md:space-y-0 md:flex-row md:justify-between md:items-center">
+      <div className="container mx-auto flex flex-col py-28 pb-48 px-5 space-y-12 md:space-y-0 md:flex-row md:justify-between md:items-center">
         <div className="md:w-5/12 space-y-3">
           <h5 className="font-signika-negative font-bold text-4xl text-mygreen-500 uppercase lg:text-5xl">
             Gimana cara pakainya ya, minaf?
